67dcb88c4fc96063777bbd678ad40bbc4d044b4d
Jordan Boggs <boggsjp@gmail.com>
2018-02-11 10:36:16 -0700

    Matching algorithm works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,18 +5,16 @@ const path = require('path');
 const friends = require('../data/friends.js');
 
 // Routing
-// module.exports = function(app) {
-  // app.get("/api/friends", function(req, res) {
-  //   res.json(friends);
-  // });
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-  // app.post("/api/new", function(req, res) {
-  //   let answers = req.body;
-  //   // function that matches your answers with each character
-  //   matchCharacter(answers);
-  // });
-
-  let answers = [5,4,3,2,1,1,2,3,4,5];
+  app.post("/api/new", function(req, res) {
+    let answers = req.body;
+    // function that matches your answers with each character
+    matchCharacter(answers);
+  });
 
   function matchCharacter(answers) {
     // This array will hold the total difference for each character
@@ -40,10 +38,18 @@ const friends = require('../data/friends.js');
     });
 
     // Find the lowest match. Lower index is tie-breaker
-    tests.forEach(number => {
-      
-    });
+    let match = false;
+    let bestMatch = 0;
+    do {
+      for (let i = 0; i < tests.length; i++) {
+        if (tests[i] === bestMatch) {
+          // console.log("The match is " + friends[i].name);
+          match = true;
+          return;
+        }
+      }
+      bestMatch++;
+    }
+    while (!match);
   }
-
-  matchCharacter(answers);
-// };
+};
