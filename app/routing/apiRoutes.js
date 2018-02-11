746d68ddb23170323e5e4b6b83d349194e4a1c3f
Jordan Boggs <boggsjp@gmail.com>
2018-02-11 10:27:53 -0700

    Working on matching algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,45 @@ const path = require('path');
 const friends = require('../data/friends.js');
 
 // Routing
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
-};
+// module.exports = function(app) {
+  // app.get("/api/friends", function(req, res) {
+  //   res.json(friends);
+  // });
+
+  // app.post("/api/new", function(req, res) {
+  //   let answers = req.body;
+  //   // function that matches your answers with each character
+  //   matchCharacter(answers);
+  // });
+
+  let answers = [5,4,3,2,1,1,2,3,4,5];
+
+  function matchCharacter(answers) {
+    // This array will hold the total difference for each character
+    // Each index matches to an index in friends array
+    let tests = [];
+
+    // Let's run through each character in friends and compare
+    friends.forEach(person => {
+      const scores = person.scores;
+      // Keep track of total difference
+      let totalDifference = 0;
+
+      // Compare each score
+      for (let i = 0; i < answers.length; i++) {
+        let difference = Math.abs(answers[i] - scores[i]);
+        totalDifference += difference;
+      }
+
+      // Push totalDifference to tests array
+      tests.push(totalDifference);
+    });
+
+    // Find the lowest match. Lower index is tie-breaker
+    tests.forEach(number => {
+      
+    });
+  }
+
+  matchCharacter(answers);
+// };
