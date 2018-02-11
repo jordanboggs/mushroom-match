bf2fb6923d084845122367a5b87d489e71e6784a
Jordan Boggs <boggsjp@gmail.com>
2018-02-10 20:06:03 -0700

    Planned out quiz question bank & answers

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+/*
+------------------------
+List of Mario Characters
+------------------------
+CHARACTER     SCORES
+------------------------
+Question  |   1 2 3 4 5 6 7 8 9 10
+----------------------------------
+Mario     |   3 2 1 5 5 1 3 1 1 1
+Luigi     |   3 3 4 5 5 1 3 4 4 1
+Wario     |   2 1 5 2 4 5 1 3 1 4
+Waluigi   |   2 1 3 3 4 5 2 2 4 5
+Bowser    |   5 1 5 1 1 5 4 5 1 3
+Yoshi     |   4 3 2 4 1 1 3 1 5 2
+Peach     |   3 5 5 5 3 1 5 5 3 1
+Toad      |   2 4 4 4 1 1 4 5 4 4
+Birdo     |   1 3 2 1 1 3 5 1 2 3
+Teresa    |   1 2 4 3 2 3 2 1 3 2
+----------------------------------
+
+----------------------------------------------------------
+List of Questions
+----------------------------------------------------------
+   1           2          3        4         5
+Strongly    Disagree    Neutral   Agree    Strongly
+Disagree                                   Agree
+----------------------------------------------------------
+1)  I prefer hot climates to cold ones
+2)  I often find myself in situations I can't get out of
+3)  I feel most at home in a castle
+4)  I often play sports with people I call my friends
+5)  I come from Italy, probably
+6)  If there's a scheme, I'm in on it
+7)  I consider myself high-class and fancy
+8)  I am talkative with a lot to say
+9)  I'd rather play a supportive role than lead the way
+10) Waaaaaaaaaah!
+----------------------------------------------------------
+*/
