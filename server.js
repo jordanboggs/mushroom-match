2653076e6efc53710143508bed22d548a244ef9c
Jordan Boggs <boggsjp@gmail.com>
2018-02-13 17:20:41 -0700

    Was this just a capitalization issue?

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Set up Express
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
